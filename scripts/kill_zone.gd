6532d32b8c15d91bbbd04609fdf0c2fde1fecc31
Nathan Lynch <nathanlynch1844@gmail.com>
2024-07-03 04:31:33 -0500

    7/3/24

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 @onready var timer = $Timer
-
+@onready var animated_sprite = $AnimatedSprite2D
 func _on_body_entered(body):
 	print("You dead")
+	Engine.time_scale = 0.1
 	timer.start()
 
 
 func _on_timer_timeout():
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
